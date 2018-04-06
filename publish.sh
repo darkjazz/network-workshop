b833ff5a91eb2636d2ac5e10ff27eee1a059f870
rewire <rewire@anrita.local>
2018-04-06 12:41:06 +0100

    updated publish script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+rm rewire-workshop.zip
+sudo rm -r rewire-workshop
 mkdir -p rewire-workshop
-# mkdir rewire-workshop/
 cp -r network-workshop-extensions rewire-workshop/
 cp d1-s1-basics.scd rewire-workshop/1-basics-sequencing.scd
 cp d1-s2-networking.scd rewire-workshop/2-synthesis-network.scd
 cp d2-s2-listen.scd rewire-workshop/3-trigger-listening.scd
 cp send-me-code.scd rewire-workshop/
 zip rewire-workshop.zip rewire-workshop
+sudo mkdir -p /Library/WebServer/Documents/rewire
+sudo cp rewire-workshop.zip /Library/WebServer/Documents/rewire/
