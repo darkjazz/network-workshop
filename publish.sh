5129cb72f0624c70ecaf326ff1ea1fa96d894308
rewire <rewire@anrita.local>
2018-04-06 20:27:36 +0100

    added extra stuff

--- publish.sh
+++ publish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-rm rewire-workshop.zip
-sudo rm -r rewire-workshop
-mkdir -p rewire-workshop
-cp -r network-workshop-extensions rewire-workshop/
-cp d1-s1-basics.scd rewire-workshop/1-basics-sequencing.scd
-cp d1-s2-networking.scd rewire-workshop/2-synthesis-network.scd
-cp d2-s2-listen.scd rewire-workshop/3-trigger-listening.scd
-cp send-me-code.scd rewire-workshop/
-cd rewire-workshop/
-zip -r -X ../rewire-workshop.zip ./*
+rm network-workshop.zip
+sudo rm -r network-workshop
+mkdir -p network-workshop
+cp -r network-workshop-extensions network-workshop/
+cp d1-s1-basics.scd network-workshop/1-basics-sequencing.scd
+cp d1-s2-networking.scd network-workshop/2-synthesis-network.scd
+cp d2-s2-listen.scd network-workshop/3-trigger-listening.scd
+cp send-me-code.scd network-workshop/
+cd network-workshop/
+zip -r -X ../network-workshop.zip ./*
 cd ..
 sudo mkdir -p /Library/WebServer/Documents/rewire
-sudo cp rewire-workshop.zip /Library/WebServer/Documents/rewire/
+sudo cp network-workshop.zip /Library/WebServer/Documents/rewire/
