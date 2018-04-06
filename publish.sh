7a572a5a9d7a3ea0d888278f4c97882e148c3bcc
rewire <rewire@anrita.local>
2018-04-06 19:06:42 +0100

    updated publish

--- publish.sh
+++ publish.sh
@@ -7,6 +7,8 @@ cp d1-s1-basics.scd rewire-workshop/1-basics-sequencing.scd
 cp d1-s2-networking.scd rewire-workshop/2-synthesis-network.scd
 cp d2-s2-listen.scd rewire-workshop/3-trigger-listening.scd
 cp send-me-code.scd rewire-workshop/
-zip rewire-workshop.zip rewire-workshop
+cd rewire-workshop/
+zip -r -X ../rewire-workshop.zip ./*
+cd ..
 sudo mkdir -p /Library/WebServer/Documents/rewire
 sudo cp rewire-workshop.zip /Library/WebServer/Documents/rewire/
